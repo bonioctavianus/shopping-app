5c3f6ec1a940493cef1e12e7ed9a2796e9f88cec
bonioctavianus <boni.oxx@gmail.com>
2020-05-07 20:04:06 +0700

    create 'AuthFragment' destination

---
+++ app/src/main/java/com/bonioctavianus/android/shopping_app/ui/auth/AuthFragment.kt
@@ -0,0 +1,18 @@
+package com.bonioctavianus.android.shopping_app.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bonioctavianus.android.shopping_app.R
+
+class AuthFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_auth, container, false)
+    }
+}
