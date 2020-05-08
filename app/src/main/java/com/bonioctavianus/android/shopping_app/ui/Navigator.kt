c87725723f4a8613a433cbe064b1685c57e882d0
bonioctavianus <boni.oxx@gmail.com>
2020-05-08 15:39:07 +0700

    create navigation from SplashFragment

---
+++ app/src/main/java/com/bonioctavianus/android/shopping_app/ui/Navigator.kt
@@ -0,0 +1,26 @@
+package com.bonioctavianus.android.shopping_app.ui
+
+import androidx.navigation.fragment.findNavController
+import com.bonioctavianus.android.shopping_app.ui.auth.AuthFragment
+import com.bonioctavianus.android.shopping_app.ui.auth.AuthFragmentDirections
+import com.bonioctavianus.android.shopping_app.ui.splash.SplashFragment
+import com.bonioctavianus.android.shopping_app.ui.splash.SplashFragmentDirections
+import javax.inject.Inject
+
+class Navigator @Inject constructor() {
+
+    fun navigateToAuthFragment(fragment: SplashFragment) {
+        val direction = SplashFragmentDirections.actionSplashFragmentToAuthFragment()
+        fragment.findNavController().navigate(direction)
+    }
+
+    fun navigateToMainFragment(fragment: SplashFragment) {
+        val direction = SplashFragmentDirections.actionSplashFragmentToMainFragment()
+        fragment.findNavController().navigate(direction)
+    }
+
+    fun navigateToMainFragment(fragment: AuthFragment) {
+        val direction = AuthFragmentDirections.actionAuthFragmentToMainFragment()
+        fragment.findNavController().navigate(direction)
+    }
+}
--- app/src/main/java/com/bonioctavianus/android/shopping_app/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/bonioctavianus/android/shopping_app/ui/splash/SplashFragment.kt
@@ -1,32 +1,32 @@
 package com.bonioctavianus.android.shopping_app.ui.splash
 
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.bonioctavianus.android.shopping_app.R
 import com.bonioctavianus.android.shopping_app.repository.user.UserService
-import com.bonioctavianus.android.shopping_app.repository.user.UserServiceV1
+import com.bonioctavianus.android.shopping_app.ui.Navigator
+import dagger.android.support.AndroidSupportInjection
+import javax.inject.Inject
 
 class SplashFragment : Fragment() {
 
-    private val mUserService: UserService = UserServiceV1()
+    @Inject
+    lateinit var mNavigator: Navigator
+    @Inject
+    lateinit var mUserService: UserService
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        if (mUserService.isUserSignedIn()) {
-            val direction = SplashFragmentDirections.actionSplashFragmentToAuthFragment()
-            findNavController().navigate(direction)
+        if (!mUserService.isUserSignedIn()) {
+            mNavigator.navigateToAuthFragment(this)
+        } else {
+            mNavigator.navigateToMainFragment(this)
         }
     }
 }
