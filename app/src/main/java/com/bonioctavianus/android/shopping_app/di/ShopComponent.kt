e6a92d3a5497d8faff86f7482524199f239a9513
bonioctavianus <boni.oxx@gmail.com>
2020-05-08 15:40:19 +0700

    register required modules

--- app/src/main/java/com/bonioctavianus/android/shopping_app/di/ShopComponent.kt
+++ app/src/main/java/com/bonioctavianus/android/shopping_app/di/ShopComponent.kt
@@ -1,6 +1,10 @@
 package com.bonioctavianus.android.shopping_app.di
 
 import com.bonioctavianus.android.shopping_app.ShopApplication
+import com.bonioctavianus.android.shopping_app.di.repository.AuthRepositoryModule
+import com.bonioctavianus.android.shopping_app.di.store.StoreModule
+import com.bonioctavianus.android.shopping_app.di.ui.FragmentModule
+import com.bonioctavianus.android.shopping_app.di.user.UserModule
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjectionModule
@@ -9,7 +13,11 @@ import javax.inject.Singleton
 @Singleton
 @Component(
     modules = [
-        AndroidInjectionModule::class
+        AndroidInjectionModule::class,
+        StoreModule::class,
+        AuthRepositoryModule::class,
+        UserModule::class,
+        FragmentModule::class
     ]
 )
 interface ShopComponent {
